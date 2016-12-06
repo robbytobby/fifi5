3e076c4d01835cf654c5aa63985fa06abe3e80ce
Widu Wittekindt <wittekindt@posteo.de>
2016-12-06 21:55:17 +0100

    introduced devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161206170535) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                              null: false
+    t.string   "username",                           null: false
+    t.string   "encrypted_password",                 null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.integer  "sign_in_count",          default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.integer  "failed_attempts",        default: 0, null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
+    t.index ["username"], name: "index_users_on_username", unique: true, using: :btree
+  end
+
 end
--- spec/features/login_is_obligatory_spec.rb
+++ spec/features/login_is_obligatory_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature 'Login is required for any Action' do
   scenario 'requests root' do
     visit ''
-    expect(current_path).to eq '/login'
-    expect(page).to have_text('Login')
+    expect(current_path).to eq '/users/sign_in'
+    expect(page).to have_text('Log in')
   end
 end

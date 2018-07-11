02a218a6007e6e18589bfd5d1e52b206154e98cb
Widu Wittekindt <christian.wittekindt@physchem.uni-freiburg.de>
2018-07-11 13:35:57 +0200

    removed line breaks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,
-         :recoverable, :trackable, :validatable,
-         :lockable, :timeoutable
+  devise :database_authenticatable, :recoverable, :trackable, :validatable, :lockable, :timeoutable
 end

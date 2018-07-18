c96b54d8660a6f16b2dd49767a8ae6a59a4d5190
Widu Wittekindt <christian.wittekindt@physchem.uni-freiburg.de>
2018-07-18 13:44:24 +0200

    generate stylesheets with sass

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module Fifi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # use sass instead of SCSS
+    config.sass.preferred_syntax = :sass
     # Settings in config/environments/* take precedence over those specified
     # here.
     # Application configuration can go into files in config/initializers

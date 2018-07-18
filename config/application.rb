e8698f83e90fa62b9f9fd0bf3a6ff26c8015fc8a
Widu Wittekindt <christian.wittekindt@physchem.uni-freiburg.de>
2018-07-18 10:41:30 +0200

    ganz frisch aufgeseztes rails

--- config/application.rb
+++ config/application.rb
@@ -1,30 +1,19 @@
 require_relative 'boot'
 
-require 'rails'
-# Pick the frameworks you want:
-require 'active_model/railtie'
-require 'active_job/railtie'
-require 'active_record/railtie'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'action_view/railtie'
-# require 'action_cable/engine'
-require 'sprockets/railtie'
-# require 'rails/test_unit/railtie'
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Fifi5
+module Fifi
   class Application < Rails::Application
-    # Use the responders controller from the responders gem
-    config.app_generators.scaffold_controller :responders_controller
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-    config.sass.preferred_syntax = :sass
-    config.sass.line_comments = true
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
--- spec/features/login_is_obligatory_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.feature 'Login is required for any Action' do
-  scenario '- application root /' do
-    visit ''
-    expect(current_path).to eq '/users/sign_in'
-    expect(page).to have_text('Log in')
-  end
-end

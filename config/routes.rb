e65d6a975db3f146f36e79ac47aaaaf87be46e23
Widu Wittekindt <christian.wittekindt@physchem.uni-freiburg.de>
2018-07-30 18:59:48 +0200

    code style

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,10 @@ Rails.application.routes.draw do
   put 'role/update'
   devise_for :users, skip: %i[registrations]
   as :user do
-    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
-    put 'users' => 'devise/registrations#update', :as => 'user_registration' 
+    get 'users/edit' => 'devise/registrations#edit', as: 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', as: 'user_registration'
   end
   get 'dashboard/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'dashboard#index'
 end
